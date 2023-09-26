a6ba277803b3d5e619f367e7d5c11e706a8c9b91
Seth Maño <joshuaseth11@gmail.com>
2023-09-26 22:33:16 +0800

    Added Progress component from shadcn

--- components/property/valuation/index.tsx
+++ components/property/valuation/index.tsx
@@ -9,14 +9,18 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
+import { Progress } from "@/components/ui/progress";
 import PropertyDetails from "./propertyDetails";
+
 
 const Valuation: React.FC = () => {
   return (
-    <Card className="w-full h-screen overflow-y-auto rounded-t-3xl rounded-b-none">
-      <CardHeader>
-        <CardTitle>Card Title</CardTitle>
-        <CardDescription>Card Description</CardDescription>
+    <Card className="relative w-full h-[calc(100vh)] overflow-y-auto rounded-t-3xl rounded-b-none">
+      <div className="p-6 sticky top-0 left-0 h-16 w-full bg-white z-20">
+        <Progress value={23} />
+      </div>
+      <CardHeader className="">
+        <CardTitle>Tell us about your property</CardTitle>
       </CardHeader>
       <CardContent>
         <PropertyDetails />
