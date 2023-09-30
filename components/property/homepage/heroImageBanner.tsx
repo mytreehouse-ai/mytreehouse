1a4c5156326a56aa6c4a035806f934e321440c6b
Gelo <chrisgelosulit@gmail.com>
2023-09-30 15:58:24 +0800

    Fix main banner image file extension and update HeroImageBanner component

--- components/property/homepage/heroImageBanner.tsx
+++ components/property/homepage/heroImageBanner.tsx
@@ -4,7 +4,7 @@ const HeroImageBanner = () => {
   return (
     <Image
       className="brightness-50 lg:rounded-2xl"
-      src="/main_banner.avif"
+      src="/main_banner.jpg"
       alt="home_page_main_banner"
       sizes="(max-width: 768px) 100vw"
       priority={true}
