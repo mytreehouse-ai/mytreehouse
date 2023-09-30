cf8f1baa062403b04295906e787fbf6320d1f5bf
Seth Maño <joshuaseth11@gmail.com>
2023-09-30 10:56:02 +0800

    Refactor layout for contact us

--- app/contact-us/page.tsx
+++ app/contact-us/page.tsx
@@ -1,10 +1,25 @@
 import ContactCard from "@/components/property/contact/contactCard";
+import Image from "next/image";
 
 const page = async () => {
 
   return (
-    <main className="md:w-1/4 mx-auto md:py-10 ">
-      <ContactCard />
+    <main className="relative h-[calc(100vh)] overflow-y-auto">
+
+      <Image
+        className="absolute opacity-90 object-cover w-full"
+        src="/satellite-map-bg.png"
+        alt="satellite_map_bg"
+        sizes="(max-width: 1200px)  100vw"
+        objectFit=""
+        priority={true}
+        fill={true}
+      />
+      <section className="absolute z-10 w-full h-full flex items-center backdrop-blur-sm">
+        <div className=" md:w-1/4 mx-auto md:py-10 ">
+          <ContactCard />
+        </div>
+      </section>
     </main>
   )
 };
