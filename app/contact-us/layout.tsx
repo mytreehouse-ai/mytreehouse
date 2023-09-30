9f6a30187d7e9f91fecb6fe378fe744fe26d40fe
Seth Maño <joshuaseth11@gmail.com>
2023-09-30 11:00:23 +0800

    Added layout.tsx on contact page

---
+++ app/contact-us/layout.tsx
@@ -0,0 +1,15 @@
+import Footer from "@/components/layouts/footer";
+import Navbar from "@/components/layouts/navbar";
+import { PropsWithChildren } from "react";
+
+const layout: React.FC<PropsWithChildren> = ({ children }) => {
+    return (
+        <main className="z-50 flex flex-col justify-between h-screen">
+            <Navbar shadowMd />
+            {children}
+            <Footer />
+        </main>
+    );
+};
+
+export default layout;
