594a323eced6e683abd4f150b16e66aa78f9ddb3
Gelo <chrisgelosulit@gmail.com>
2023-09-30 15:46:36 +0800

    Add @vercel/analytics for tracking page views

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { Analytics } from "@vercel/analytics/react";
 import ReactQueryProvider from "@/components/provider/ReactQueryProvider";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -19,6 +20,7 @@ export default function RootLayout({
     <ReactQueryProvider>
       <html lang="en">
         <body className={inter.className}>{children}</body>
+        <Analytics />
       </html>
     </ReactQueryProvider>
   );
