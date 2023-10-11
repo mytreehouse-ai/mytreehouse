40393429b80d0a48c6a2f8d11274eae2ba5abab9
Seth Maño <joshuaseth11@gmail.com>
2023-10-11 22:34:06 +0800

    Fixed property details images placement

--- components/property/listing/properties/details/propertyimagesplaceholder.tsx
+++ components/property/listing/properties/details/propertyimagesplaceholder.tsx
@@ -1,13 +1,17 @@
 'use client'
 
-import React from 'react'
+import React, { useState } from 'react'
 import Image from 'next/image'
+import { cn } from '@/lib/utils'
 
 interface PropertyImagesPlaceholderProps {
     images?: string[]
 }
 
 const PropertyImagesPlaceholder = ({images}:PropertyImagesPlaceholderProps) => {
+    const [imgShowcase, setImgShowcase] = useState("");
+
+console.log(images?.length)
 
   return (
          <div className="flex flex-col lg:flex-row gap-x-2 gap-y-2">
@@ -21,11 +25,11 @@ const PropertyImagesPlaceholder = ({images}:PropertyImagesPlaceholderProps) => {
             priority={true}
           />
         </div>
-        <div className="w-full lg:w-1/2 ">
+        <div className={cn(images && images?.length > 2 ? "block" : "hidden","w-full lg:w-1/2 ")}>
         {images && images?.length >= 3 && (
           <div className="relative grid grid-cols-2 gap-2 h-64 lg:h-full " >
             {images?.slice(1, 4).map((image) => (
-              <div key={image} className="relative w-full h-full overflow-clip rounded-md last:col-span-2">
+              <div key={image} className="relative w-full h-full overflow-clip rounded-md">
               <Image
                 src={image}
                 className="h-[250px] rounded-md object-cover hover:cursor-pointer hover:scale-110 transition ease-in-out "
