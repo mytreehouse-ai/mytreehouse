0a488b5bcf6a326665e79e96cdaac38bfdc48d4d
csulit <chrisgelosulit@gmail.com>
2023-10-14 19:12:05 +0800

    Refactor ReactQueryProvider to remove ReactQueryDevtools

--- components/provider/ReactQueryProvider.tsx
+++ components/provider/ReactQueryProvider.tsx
@@ -1,16 +1,12 @@
 "use client";
 import { PropsWithChildren } from "react";
 import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 const queryClient = new QueryClient();
 
 const ReactQueryProvider: React.FC<PropsWithChildren> = ({ children }) => {
   return (
-    <QueryClientProvider client={queryClient}>
-      {children}
-      <ReactQueryDevtools initialIsOpen={false} />
-    </QueryClientProvider>
+    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
   );
 };
 
