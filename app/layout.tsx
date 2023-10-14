e885873922eebd2e6f934b4c0c9e34d3bc6397cb
csulit <chrisgelosulit@gmail.com>
2023-10-14 19:15:16 +0800

    Add Analytics component to RootLayout

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,8 +23,8 @@ export default function RootLayout({
         <body className={inter.className}>
           {children}
           <ReactQueryDevtools initialIsOpen={false} />
+          <Analytics />
         </body>
-        <Analytics />
       </html>
     </ReactQueryProvider>
   );
