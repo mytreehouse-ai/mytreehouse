28e305f60ba7d28e6707ef20423534113bca4b05
csulit <chrisgelosulit@gmail.com>
2023-10-15 01:46:20 +0800

    Update property contact card component and background images

--- components/property/homepage/heroimagebanner.tsx
+++ components/property/homepage/heroimagebanner.tsx
@@ -5,9 +5,9 @@ import { imageKitLoader } from "@/lib/utils";
 const HeroImageBanner = () => {
   return (
     <Image
-      className="brightness-50 lg:rounded-2xl"
+      className="brightness-75 lg:rounded-2xl"
       loader={imageKitLoader}
-      src="mytreehouse/main-homepage-banner.jpg"
+      src="mytreehouse/main_banner_by_dalle3.png"
       alt="home_page_main_banner"
       sizes="(max-width: 768px) 100vw, 700px"
       priority={true}
