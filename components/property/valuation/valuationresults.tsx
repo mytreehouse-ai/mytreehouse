4efa2defd16cccccc331ce1fd575fef20291a9b8
Seth Maño <146793544+sethmano@users.noreply.github.com>
2023-10-17 21:31:08 +0800

    Added valuation data result to UI

--- components/property/valuation/valuationresults.tsx
+++ components/property/valuation/valuationresults.tsx
@@ -4,6 +4,7 @@ import ValuationStepper from "@/hooks/useStepperStore";
 import useValuationFormStore from "@/hooks/useValuationFormStore";
 import SuccessResultImage from "./successresultimage";
 import { useGetValuationResultHook } from "@/hooks/useGetValuationResultHook";
+import { Separator } from "@/components/ui/separator";
 
 const ValuationResults: React.FC = () => {
   const { setCurrentStepIndex } = ValuationStepper();
@@ -17,11 +18,12 @@ const ValuationResults: React.FC = () => {
     propertyType: propertyDetailValues.propertyType,
   });
 
-  console.log("VALUATION HOOK RESULT", valuationData);
+  const forSaleValue = valuationData?.phpFormat.withoutClosedTransaction.forSale;
+  const forRentValue = valuationData?.phpFormat.withoutClosedTransaction.forRent
 
   return (
     <>
-      <div className="relative mx-auto h-48 w-48 md:h-72 md:w-72">
+      <div className="relative mx-auto h-40 w-40 md:h-60 md:w-60">
         <SuccessResultImage />
       </div>
       <div>
@@ -32,16 +34,26 @@ const ValuationResults: React.FC = () => {
       <div className="mt-4 space-y-4">
         <section className="rounded-md border p-4">
           <h1 className="w-full font-bold text-neutral-800">For sale value</h1>
-          <span className="mt-1 flex justify-between">
-            <p className="text-xs text-neutral-600">Estimated value:</p>
-            <p className="text-xs font-bold text-neutral-800">₱ 124,415,000</p>
+          <span className="mt-1 flex justify-between items-end">
+            <p className=" text-neutral-600 text-sm">Estimated value:</p>
+            <p className="font-bold text-emerald-700">{forSaleValue?.pricePerSqm}</p>
+          </span>
+          <Separator className="my-2"/>
+            <span className="mt-4 flex justify-between items-end">
+            <p className=" text-neutral-600 text-sm">Appraisal value:</p>
+            <p className="font-bold text-emerald-700">{forSaleValue?.appraisalValue}</p>
           </span>
         </section>
         <section className="rounded-md border p-4">
           <h1 className="w-full font-bold text-neutral-800">For rent value</h1>
-          <span className="mt-1 flex justify-between">
-            <p className="text-xs text-neutral-600">Estimated value:</p>
-            <p className="text-xs font-bold text-neutral-800">₱ 124,415,000</p>
+          <span className="mt-4 flex justify-between">
+          <p className=" text-neutral-600 text-sm">Estimated value:</p>
+          <p className="font-bold text-emerald-700">{forRentValue?.pricePerSqm}</p>
+          </span>
+                 <Separator className="my-2"/>
+          <span className="mt-1 flex justify-between items-end ">
+            <p className=" text-neutral-600 text-sm">Appraisal value:</p>
+            <p className="font-bold text-emerald-700">{forRentValue?.appraisalValue}</p>
           </span>
         </section>
       </div>
