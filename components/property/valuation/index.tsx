19e1b833d642cd29a22baaa95b295d77b22a34bf
Seth Maño <146793544+sethmano@users.noreply.github.com>
2023-10-17 21:47:53 +0800

    Adjusted layout for mobile on contact us page

--- components/property/valuation/index.tsx
+++ components/property/valuation/index.tsx
@@ -40,7 +40,7 @@ const Valuation: React.FC = () => {
     ] || null;
 
   return (
-    <Card className="relative w-full overflow-y-auto rounded-t-3xl md:rounded-xl">
+    <Card className="relative w-full overflow-y-auto rounded-xl md:rounded-xl">
       <div className="sticky left-0 top-0 z-10 flex h-20 w-full items-center justify-center bg-white px-6">
         <Progress
           value={
