72e0881e67e3baf80d9afdb7718e0bafbe374621
csulit <chrisgelosulit@gmail.com>
2023-10-22 16:17:01 +0800

    refactor: Simplify text search query in property listing search route

--- app/api/properties/listing/search/route.ts
+++ app/api/properties/listing/search/route.ts
@@ -54,10 +54,7 @@ export async function GET(req: Request) {
             ${
               queryParams.data?.text_search
                 ? `
-            ,ts_rank(to_tsvector('english', p.listing_title || ' ' || coalesce(p.ts_query_listing_type_name, '') || ' ' || coalesce(p.ts_query_property_type_name, '') || ' ' || coalesce(p.address, '') || ' ' || coalesce(p.ts_query_city_name, '') || ' ' || coalesce(p.description, '')), to_tsquery('english', '${queryParams.data.text_search.replace(
-              /\s+/g,
-              " | ",
-            )}')) as rank
+            ,ts_rank(tsv, plainto_tsquery('english', '${queryParams.data.text_search}')) as rank
             `
                 : ``
             }
@@ -76,10 +73,7 @@ export async function GET(req: Request) {
           ${
             queryParams.data?.text_search
               ? `
-          and to_tsvector('english', p.listing_title || ' ' || coalesce(p.ts_query_listing_type_name, '') || ' ' || coalesce(p.ts_query_property_type_name, '') || ' ' || coalesce(p.address, '') || ' ' || coalesce(p.ts_query_city_name, '') || ' ' || coalesce(p.description, '')) @@ to_tsquery('english', '${queryParams.data.text_search.replace(
-            /\s+/g,
-            " | ",
-          )}')
+          and tsv @@ plainto_tsquery('english', '${queryParams.data.text_search}')
           `
               : ``
           }
