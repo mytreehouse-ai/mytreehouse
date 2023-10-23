4f6566beb96a6f23cebf206dd9266d59b5156304
csulit <chrisgelosulit@gmail.com>
2023-10-23 13:51:24 +0800

    feat: Add new components for live chat layout and page

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,5 +1,20 @@
-const NotFound = () => {
-  return <div>Not found</div>;
-};
+import { Button } from "@/components/ui/button";
 
-export default NotFound;
+export default function Component() {
+  return (
+    <main className="flex h-screen flex-col items-center justify-center bg-gray-100 px-4 text-center dark:bg-gray-800 sm:px-6 lg:px-8">
+      <h1 className="text-5xl font-bold text-gray-900 dark:text-gray-100">
+        404 Not Found
+      </h1>
+      <p className="mt-8 text-lg text-gray-700 dark:text-gray-300">
+        {`Oops! The page you're looking for does not exist. It might have been
+        moved or deleted.`}
+      </p>
+      <div className="mt-6">
+        <Button className="rounded-md px-4 py-2 text-white">
+          Go Back Home
+        </Button>
+      </div>
+    </main>
+  );
+}
