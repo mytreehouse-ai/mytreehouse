0277a7bbe982ef3611461725bb943367a01f9dbf
jomielandrade <jomielandrade@gmail.com>
2023-11-04 22:19:16 +0800

    RF: Comment getUserOrRedirect

--- app/(admin)/cms/page.tsx
+++ app/(admin)/cms/page.tsx
@@ -4,7 +4,7 @@ import { getUserOrRedirect } from "@propelauth/nextjs/server/app-router";
 import Test from "@/components/cms/test";
 
 const page = async () => {
-  const user = await getUserOrRedirect();
+  // const user = await getUserOrRedirect();
 
   return (
     <>
