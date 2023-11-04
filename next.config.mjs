5e0fe7e4384b2efa983e83db6e8fec7cfba14ec4
csulit <chrisgelosulit@gmail.com>
2023-11-04 14:07:11 +0800

    chore: Add experimental webpackBuildWorker configuration

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,9 @@ await import("./lib/env.mjs");
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  experimental: {
+    webpackBuildWorker: true,
+  },
   images: {
     remotePatterns: [
       {
