73e161afe9c2ac3ff4ad18462cf65c0bf5c965f0
jomielandrade <jomielandrade@gmail.com>
2023-11-16 00:06:34 +0800

    NF: Added mobile navbar

--- components/layouts/navbar.tsx
+++ components/layouts/navbar.tsx
@@ -1,40 +1,68 @@
 "use client";
+
+import { useState } from "react";
 import Link from "next/link";
 
 const Navbar: React.FC = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
-    <div className="fixed left-0 top-0 z-50 w-full bg-white p-4 text-slate-800">
-      <div className="container mx-auto">
-        <div className="flex items-center justify-center">
-          <div className="flex space-x-4">
-            <Link
-              href="/"
-              className="text-base font-semibold text-slate-800 hover:text-emerald-600"
-            >
-              Home
-            </Link>
-            <Link
-              href="/property-listings"
-              className="text-base font-semibold text-slate-800 hover:text-emerald-600"
-            >
-              Listings
-            </Link>
-            <Link
-              href="/property-valuation"
-              className="text-base font-semibold text-slate-800 hover:text-emerald-600"
-            >
-              Valuation
-            </Link>
-            <Link
-              href="/contact-us"
-              className="text-base font-semibold text-slate-800 hover:text-emerald-600"
-            >
-              Contact
-            </Link>
-          </div>
+    <nav className="fixed top-0 z-50 flex w-full flex-wrap items-center justify-between bg-white p-6">
+      <div className="z-20 mr-6 flex flex-shrink-0 items-center justify-between text-emerald-600">
+        <Link href="/">
+          <span className="text-xl font-semibold tracking-tight">
+            MyTreeHouse
+          </span>
+        </Link>
+      </div>
+      <div className="block md:hidden">
+        <button
+          onClick={() => setIsOpen(!isOpen)}
+          className="flex items-center rounded border border-emerald-600 px-3 py-2 text-emerald-600 hover:border-emerald-600 hover:text-emerald-600"
+        >
+          <svg
+            className="h-3 w-3 fill-current"
+            viewBox="0 0 20 20"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <title>Menu</title>
+            <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
+          </svg>
+        </button>
+      </div>
+      <div
+        className={`${
+          isOpen ? "z-50 block" : "hidden"
+        } block w-full flex-grow md:flex md:w-auto md:items-center md:justify-end `}
+      >
+        <div className="text-sm font-semibold md:flex-grow">
+          <Link
+            href="/"
+            className="mr-4 mt-4 block hover:text-emerald-600 md:mt-0 md:inline-block"
+          >
+            Home
+          </Link>
+          <Link
+            href="/property-listings"
+            className="mr-4 mt-4 block hover:text-emerald-600 md:mt-0 md:inline-block"
+          >
+            Listings
+          </Link>
+          <Link
+            href="/property-valuation"
+            className="mr-4 mt-4 block hover:text-emerald-600 md:mt-0 md:inline-block"
+          >
+            Valuation
+          </Link>
+          <Link
+            href="/contact-us"
+            className="mt-4 block hover:text-emerald-600 md:mt-0 md:inline-block"
+          >
+            Contact
+          </Link>
         </div>
       </div>
-    </div>
+    </nav>
   );
 };
 
