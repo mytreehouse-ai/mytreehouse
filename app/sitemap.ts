13f3516e0114ca0ff1f66dff8711c2b15e7e5a53
jomielandrade <jomielandrade@gmail.com>
2023-12-04 22:30:59 +0800

    Refactor sitemap function to handle fetch errors and non-array data
    In the sitemap.ts file, the sitemap function has been modified to handle
    errors that may occur during the API fetch call. Previously, any fetch
    error would have been unhandled, potentially causing unexpected
    behavior. Now, if an error occurs, it is caught and logged to the
    console, allowing for better error handling.
    
    Additionally, the function now checks if the returned data from the API
    is an array. If it is not, an error is thrown to indicate that the data
    format is unexpected.
    
    These changes improve the reliability and robustness of the sitemap
    function by handling potential errors and validating the data format.

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,18 +1,55 @@
 import { Property } from "@/interface/property";
 
+// export default async function sitemap() {
+//   const res = await fetch(
+//     `https://beta.mytree.house/api/properties/listing/search`,
+//   );
+
+//   const allPropertyListings: Property[] = await res.json();
+
+//   const propertyListings = allPropertyListings?.map((propertyListing) => ({
+//     url: `https://beta.mytree.house/property-listings/details/${propertyListing.property_id}`,
+//     lastModified: new Date().toISOString(),
+//   }));
+
+//   const routes = ["/property-valuation", "/contact-us"]?.map((route) => ({
+//     url: `https://beta.mytree.house${route}`,
+//     lastModified: new Date().toISOString(),
+//   }));
+
+//   return [...propertyListings, ...routes];
+// }
+
 export default async function sitemap() {
-  const res = await fetch(
-    `https://beta.mytree.house/api/properties/listing/search`,
-  );
+  let allPropertyListings: Property[] = [];
 
-  const allPropertyListings: Property[] = await res.json();
+  try {
+    const res = await fetch(
+      `https://beta.mytree.house/api/properties/listing/search`,
+    );
 
-  const propertyListings = allPropertyListings?.map((propertyListing) => ({
+    if (!res.ok) {
+      throw new Error(`HTTP error! status: ${res.status}`);
+    }
+
+    const data = await res.json();
+
+    if (!Array.isArray(data)) {
+      throw new Error("Data is not an array");
+    }
+
+    allPropertyListings = data;
+  } catch (error) {
+    console.error("Fetch error: ", error);
+    // Handle error as needed, e.g. return a default sitemap or throw the error
+  }
+
+  const propertyListings = allPropertyListings.map((propertyListing) => ({
     url: `https://beta.mytree.house/property-listings/details/${propertyListing.property_id}`,
     lastModified: new Date().toISOString(),
   }));
 
-  const routes = ["/property-valuation", "/contact-us"]?.map((route) => ({
+  const routes = ["/property-valuation", "/contact-us"].map((route) => ({
     url: `https://beta.mytree.house${route}`,
     lastModified: new Date().toISOString(),
   }));
