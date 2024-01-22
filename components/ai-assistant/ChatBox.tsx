a701931db2433f2542597a15f233f5ec541b266c
csulit <chrisgelosulit@gmail.com>
2024-01-23 00:08:38 +0800

    style(ai-assistant): Adjust prose size in ChatBox component for lg screens

--- components/ai-assistant/ChatBox.tsx
+++ components/ai-assistant/ChatBox.tsx
@@ -94,7 +94,7 @@ const ChatBox = () => {
             <div className="flex items-start gap-x-2 ">
               <div className="h-6 w-6 shrink-0 rounded-full  bg-emerald-400" />
               <div className="rounded-md bg-neutral-50 px-4 py-2 shadow-sm">
-                <div className="prose">
+                <div className="prose lg:prose-sm">
                   <Markdown remarkPlugins={[remarkGfm]}>{data}</Markdown>
                 </div>
               </div>
